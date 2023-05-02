6e34a6e04202bfe776a39b988e49a8b8051ead32
SGErik <erik.souzaxbr@gmail.com>
2023-05-02 17:07:25 -0300

    feat: adicionado coluna de avatar para foto do usuário

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -156,6 +156,25 @@ const Home = () => {
   }
 
 
+  const ImageCell = ({ rowData, dataKey, ...props }) => (
+    <Cell {...props} style={{ padding: 0 }}>
+      <div
+        style={{
+          width: 40,
+          height: 40,
+          background: '#f5f5f5',
+          borderRadius: 6,
+          marginTop: 2,
+          overflow: 'hidden',
+          display: 'inline-block'
+        }}
+      >
+        <img src={rowData.url} width="40" />
+      </div>
+    </Cell>
+  );
+  
+  
   const handleDeleteModalOpen = (userId) => {
     setDeleteModal(true)
     setSelectedUser(userId)
@@ -191,8 +210,12 @@ const Home = () => {
         height={600}
         data={data}
       >
+        <Column width={90} align='center'>
+          <HeaderCell>Avatar</HeaderCell>
+          <ImageCell dataKey="url"/>
+        </Column>
 
-        <Column width={300} align='center'>
+        <Column width={200} align='center'>
           <HeaderCell>Nome</HeaderCell>
           <Cell dataKey="name" />
         </Column>
